a997c8550a4d1e3154dbfb3143d9f4c3b40d7493
pitziro <msarmientor1@gmail.com>
2024-07-02 03:45:49 -0500

    calendarOk

---
+++ src/Calendly.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { PopupButton } from 'react-calendly';
+
+const CalendlyEmbed = ({ userName, userEmail, onEventScheduled }) => {
+	const CALENDLY_URL = 'https://calendly.com/perspectivascap/agendarcita';
+
+	useEffect(() => {
+		window.addEventListener('message', (e) => {
+			if (
+				e.origin === 'https://calendly.com' &&
+				e.data.event &&
+				e.data.event.indexOf('calendly.event_scheduled') === 0
+			) {
+				onEventScheduled();
+			}
+		});
+		return () => {
+			window.removeEventListener('message', null);
+		};
+	}, [onEventScheduled]);
+
+	return (
+		<div className='App'>
+			<PopupButton
+				url={CALENDLY_URL}
+				rootElement={document.getElementById('root')}
+				text='Click here to schedule!'
+				textColor='#ffffff'
+				color='#00a2ff'
+				prefill={{ name: userName, email: userEmail }}
+			/>
+		</div>
+	);
+};
+
+export default CalendlyEmbed;
